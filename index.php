e1bc0a1417777d5b39e46b4cc5169db262bd0688
dtweety <me@daniyaalkhan.com>
2017-05-14 17:27:08 +0300

    Added DB wrapper class

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Calibr - Sign In</title>
+  </head>
+  <body>
+    <form class="" action="login.php" method="post">
+      <input type="text" name="username" value="" placeholder="Username"><br>
+      <input type="password" name="password" value="" placeholder="Password"><br>
+      <input type="submit" name="submit" value="Log In">
+    </form>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+  session_start();
+
+  require_once "app/DB.php";
+
+  $db = new DB;
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>

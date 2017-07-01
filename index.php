ed7795068e2306991df884eff69e02ceefb75ac2
dtweety <me@daniyaalkhan.com>
2017-07-02 00:03:21 +0530

    Modifications to index

--- index.php
+++ index.php
@@ -4,16 +4,28 @@
   require_once "header.php";
  ?>
  <div class="catch">
-   
+   <div class="container">
+     <div class="catch-text">
+       <div class="catch-header">
+         Hi.
+       </div>
+       <div class="catch-sub">
+         Calibr provides best in class study resource sharing.
+       </div>
+     </div>
+   </div>
  </div>
  <div class="container">
-   <div class="grade-subjects">
+   <div class="grade-subjects grade-subjects-left">
     <div class="grade-subjects-header">
-
+      Class XI Subjects
     </div>
     <div class="grade-subjects-content">
 
     </div>
+   </div>
+   <div class="grade-subjects grade-subjects-left">
+
    </div>
  </div>
 <?php

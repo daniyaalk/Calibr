dc495312bb85a59c5f0ac5681d2dc0128020d03b
dtweety <me@daniyaalkhan.com>
2017-07-02 23:48:29 +0530

    Added Links

--- header.php
+++ header.php
@@ -14,9 +14,10 @@
  <html>
    <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title><?php echo $_title; ?></title>
 
-    <link rel="stylesheet" href="css/master.css" charset="utf-8">
+    <link rel="stylesheet" href="css/main.css" charset="utf-8">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   $_title = "Home";
   require_once "header.php";
  ?>
+ <link rel="stylesheet" href="css/home.css" media="screen" title="no title" charset="utf-8">
+ <!-- TODO: Work on this this later
  <div class="catch">
    <div class="container">
      <div class="catch-text">
@@ -10,21 +12,69 @@
          Hi.
        </div>
        <div class="catch-sub">
-         Calibr provides best in class study resource sharing.
        </div>
      </div>
    </div>
  </div>
- <div class="container">
-   <div class="grade-subjects grade-subjects-left">
-    <div class="grade-subjects-header">
-      Class XI Subjects
-    </div>
-    <div class="grade-subjects-content">
-
-    </div>
-   </div>
-   <div class="grade-subjects grade-subjects-left">
+ -->
+ <div class="subject-listings">
+   <div class="container">
+     <div class="row">
+       <div class="col-xs-12 col-lg-6">
+         <div class="grade-subjects grade-subjects-left">
+          <div class="grade-subjects-header">
+            Class XI Subjects
+          </div>
+          <div class="grade-subjects-content">
+            <a href="#">
+              <div class="grade-subjects-entry">
+                Physics
+              </div>
+            </a>
+            <div class="grade-subjects-entry">
+              Chemistry
+            </div>
+            <div class="grade-subjects-entry">
+              Mathematics
+            </div>
+            <div class="grade-subjects-entry">
+              English
+            </div>
+            <div class="grade-subjects-entry">
+              Computer Science
+            </div>
+          </div>
+         </div>
+       </div>
+       <div class="col-xs-12 col-lg-6">
+         <div class="grade-subjects grade-subjects-right">
+          <div class="grade-subjects-header">
+            Class XII Subjects
+          </div>
+          <div class="grade-subjects-content">
+            <div class="grade-subjects-content">
+              <a href="#">
+                <div class="grade-subjects-entry">
+                  Physics
+                </div>
+              </a>
+              <div class="grade-subjects-entry">
+                Chemistry
+              </div>
+              <div class="grade-subjects-entry">
+                Mathematics
+              </div>
+              <div class="grade-subjects-entry">
+                English
+              </div>
+              <div class="grade-subjects-entry">
+                Computer Science
+              </div>
+            </div>
+          </div>
+         </div>
+       </div>
+     </div>
 
    </div>
  </div>

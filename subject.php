df7d8c0a1e2d80f2f5ffca7dccbd7ed843b5370c
dtweety <me@daniyaalkhan.com>
2017-07-06 18:37:55 +0530

    Added layout for subject page

--- subject.php
+++ subject.php
@@ -8,7 +8,7 @@
   $query = $db->query("SELECT s.id, s.name, s.grade, c.name, c.country, c.locale FROM subjects AS s, curriculums AS c WHERE s.id={$_GET['id']} AND c.id=s.curriculum");
 
   /*
-  ~~DEBUGGING CODE``
+  ~~DEBUGGING CODE~~
   echo "<pre>";
   var_dump($query->fetch_row());
   echo "</pre>";
@@ -21,3 +21,33 @@
 
   require_once "header.php";
  ?>
+
+<div class="container container-10-margin">
+  <ol class="breadcrumb">
+    <li><?php echo $subjectInformation[4]; ?></li>
+    <li><a href="#"><?php echo $subjectInformation[3]; ?></a></li>
+    <li><a href="#">Grade <?php echo $subjectInformation[2]; ?></a></li>
+    <li><a href="#"><?php echo $subjectInformation[1]; ?></a></li>
+  </ol>
+</div>
+<div class="container">
+  <div class="page-header">
+    <h1><?php echo $subjectInformation[1]; ?> <small>Grade <?php echo $subjectInformation[2]; ?></small></h1>
+  </div>
+  <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          <!-- TODO: Add listing of study resources-->
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+        <!-- TODO: Add listing of study resources-->
+      </div>
+    </div>
+  </div>
+</div>
+</div>

2ac883baeb3995fa8db30d9c1c1c0a45343558eb
dtweety <me@daniyaalkhan.com>
2017-07-13 21:05:45 +0530

    Changed Mailgun API key an removed it from Code.

--- app/Mail.php
+++ app/Mail.php
@@ -5,7 +5,7 @@
 
   class Mail{
 
-    private $sendingKey = "key-925b16ecb571b6e66faafa5ce8f55be0"; //Mailgun email sending key
+    private $sendingKey = ""; //Mailgun email sending key
     private $domain = "calibracademy.com";
 
     public function sendMail($to, $from, $subject, $message, $tags = array()){

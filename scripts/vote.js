d566d747818cf71b5a8f9f2db11f1d100f73081b
dtweety <me@daniyaalkhan.com>
2017-09-16 21:13:53 +0530

    Fixed #57

--- scripts/vote.js
+++ scripts/vote.js
@@ -1,12 +1,23 @@
 function vote(type, id){
 
-  $("#vote-up").removeClass("btn-info");
-  $("#vote-down").removeClass("btn-danger");
+  if(parseInt(type) == 1){
+    $("#vote-down").removeClass("btn-danger");
 
-  if(type == 1){
-    $("#vote-up").addClass('btn-info');
-  }else if(type == -1){
-    $("#vote-down").addClass('btn-danger');
+    if($("#vote-up").hasClass('btn-info')){
+      //if upvote button already has btn-info class, it implies that the user is retracting their vote.
+      $("#vote-up").removeClass('btn-info');
+    }else{
+      $("#vote-up").addClass('btn-info');
+    }
+  }else if(parseInt(type) == -1){
+    $("#vote-up").removeClass("btn-info");
+
+    if($("#vote-down").hasClass('btn-danger')){
+      //if downvote button already has btn-info class, it implies that the user is retracting their vote.
+      $("#vote-down").removeClass('btn-danger');
+    }else{
+      $("#vote-down").addClass('btn-danger');
+    }
   }
 
   $.ajax({
